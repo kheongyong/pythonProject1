6c46bed65f6c4a33e911f14aed0efabed649f931
parkh <a>
2021-07-31 16:56:50 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from tqdm.notebook import tqdm_notebook
+import numpy as np
+from pyarrow import csv
+import os
+
+folder_name = "C:\\Users\\parkh\\Documents\\과천시고압\\"
+temp_arr = ["01", "02", "03", "04"]
+for i in range(len(temp_arr)) : #매 월에 따른 처리
+    file_name = "(고압)과천시_2019"+temp_arr[i]+".csv" #파일명
+    # df = pd.read_csv(folder_name + file_name, encoding = 'cp949')
+    # df.shape
+    #print(df)
+    ##########################출력명######################
+    all_files = os.listdir(folder_name)
+
+    li = []
+    column_names = ['date', 'time', '시/도', '읍_면_동', '고객번호', '계약종별', '계약전력', '공급방식', '고저압구분', '유효전력량', '지상무효전력량', '진상무효전력량']
+    for filename in all_files:
+        df = csv.read_csv(folder_name + file_name, read_options = csv.ReadOptions(encoding = 'CP949', skip_rows = 1, column_names = column_names))
+        # df.drop_duplicates() # 중복 레코드를 제거함
+        li.append(df)
+
+    frame = pd.concat(li, axis=0, ignore_index=True)
+    frame.shape
